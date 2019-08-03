1f1960da2bb5c950d52ca66d2955943b7bf8160b
Krisztian <webevolution13@gmail.com>
2019-08-03 17:58:40 +0200

    proba feladat kesz

---
+++ js/my_java.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(document).on("scroll", function () {
+
+        if ($(document).scrollTop() > 100) {
+            $("header").removeClass("large").addClass("small");
+        } else {
+            $("header").removeClass("small").addClass("large");
+        }
+
+    });
+});
